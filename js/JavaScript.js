5e404994bcf5ae73cc0dfd04e1776714d1f14aeb
HENRIQUE GUIMARAES <118493830+bkhenrique@users.noreply.github.com>
2023-04-28 21:38:40 +0100

    js contribuição

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,20 +1,45 @@
-console.log('Bem Vindo ao inicio de JavaScript!')//aviso comum
-console.error('esse e um suposto erro')//erro em vermelho
-console.warn('esse é um warn') //alerta em amarelo
-console.info('esse é um info') //esse é um informação
+// console.log('Bem Vindo ao inicio de JavaScript!')//aviso comum
+// console.error('esse e um suposto erro')//erro em vermelho
+// console.warn('esse é um warn') //alerta em amarelo
+// console.info('esse é um info') //esse é um informação
 
-// variaveis
-var htmlEditor = document.getElementById('html-editor');
 
-function updatePreview() {
-  var preview = document.getElementById('preview');
-  preview.innerHTML = htmlEditor.value;
+
+
+//objetos
+
+const product = {
+  name: 'camisa',
+  price: 15.99,
+  inStock: true,
+  sizes: ['P', 'M', 'G'],
+  'main color':'azul',
+}
+console.log(product.name);
+console.log(product['name']);
+console.log(product['main color']);
+
+
+// JSON JavaScritp Object Notation
+
+const dog = {
+  name:'Shark',
+  age: 10,
 }
 
-htmlEditor.addEventListener('input', function() {
-  updatePreview();
+const json = JSON.stringify(dog) //converte para JSON
+console.log(json)
+
+const obj = JSON.parse(json) // convertendo json para usar quando vier de api  
+console.log(obj)
+  
+
+//extrutura condicional
+
+
+
+var editor = CodeMirror.fromTextArea(document.getElementById("html-editor"), {
+  lineNumbers: true,
+  mode: "htmlmixed",
+  theme: "monokai-dark"
 });
-
-autosize(document.querySelectorAll('.autosize'));
-
-
